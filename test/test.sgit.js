4e12fafa7f228d9456193d83fc03804738b7d9cf
Lush Zero <lushzero@derpkitten.com>
2013-09-01 17:10:57 +0000

    update tests for new log function

--- test/test.sgit.js
+++ test/test.sgit.js
@@ -9,18 +9,44 @@ describe('Node-sgit', function() {
 			assert.equal(err,'');
 		});	
 	});
-});
 
-describe('Node-sgit', function() {
 	it("should be able to commit a single file", function() {
 		exec('echo "this is a test" > ./testrepo/test.c');
 		sgit.commit_bypath('./testrepo','test.c','this is a test commit message',function(err) {
-			//assert.equal(err,'');
-			//exec('rm -rf ./testdir');
+			sgit.log('./testrepo',function(err,log) {
+				log = JSON.parse(log);
+				var firstEntry = "";
+				for (var i in log) {
+					firstEntry = i;
+					break;
+				}
+				assert(log[firstEntry].message,'this is a test commit message');
+			});
 		});
+	});
 
-		var out = exec('cd ./testrepo; /usr/bin/git log');
-		assert.notStrictEqual(out.match(/.*this is a test commit message.*/g),null);
-		exec('rm -rf ./testrepo');
+        it("should be able to log history of a repo", function() {
+                exec('echo "this is a test 2 editing this file" > ./testrepo/test.c');
+                sgit.commit_bypath('./testrepo','test.c','this is a revised commit message',function(err) {
+                        sgit.log('./testrepo',function(err,log) {
+                                log = JSON.parse(log);
+                                var loop = 0;
+                                for (var i in log) {
+					if (loop == 0) {
+                                		assert(log[i].message,'this is a test commit message');
+					}
+					else if (loop == 1) {
+                                		assert(log[i].message,'this is a revised commit message');
+					}	
+                                        loop++;
+                                }
+                        });
+                });
+
+        });
+
+        it("clean up after itself", function() {
+		exec("rm -rf ./testrepo");
+		assert.strictEqual(fs.existsSync('./testrepo'),false);
 	});
 });
