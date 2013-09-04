4a1fa621af50c3dfc78b25f8ca1b9d7832f70f75
Lush Zero <lushzero@derpkitten.com>
2013-09-04 10:00:40 +0000

    try try try travis again

--- compile_ligbit2.sh
+++ compile_ligbit2.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+wget http://sourceforge.net/projects/boost/files/boost/1.54.0/boost_1_54_0.tar.bz2
+tar --bzip2 -xf boost_1_54_0.tar.bz2
+cd boost_1_54_0
+./bootstrap.sh --help
+sudo ./bootstrap.sh
+sudo ./b2 install
+cd ..
+bash ./compile_libgit2.sh
 cd deps/libgit2
 cmake .
 make
+cd ../../
+node-gyp configure
